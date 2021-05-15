4ec222741d8c0bd3842a54ece3ecb0090e14dfbe
Moustafa <freelansoft20@gmail.com>
2021-05-15 08:16:39 +0200

    Start our Job DTO

--- app/src/main/java/freelansoft/dynasoft/dto/job.kt
+++ app/src/main/java/freelansoft/dynasoft/dto/job.kt
@@ -1,6 +1,6 @@
 package freelansoft.dynasoft.dto
 
-data class job(var genus:String, var specimen:String, var common:String) {
+data class Job(var genus:String, var specimen:String, var common:String) {
     override fun toString(): String {
         return common
     }
